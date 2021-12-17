ef7f552d4155163a766996b648aa9f3d28db8771
Nathaniel Cuadros <54486315+RatFuryJunior@users.noreply.github.com>
2021-12-17 13:06:41 -0800

    Update ReferenceView.swift

--- CPSC357_Final/Views/ReferenceView.swift
+++ CPSC357_Final/Views/ReferenceView.swift
@@ -6,7 +6,10 @@
 //
 
 import SwiftUI
-
+//Simple refrence view that just contians a block of text
+//The text has the refreces useds for the application
+//Called from the main menu
+//Links are not hyperlinked but are what we used for the main info of the application
 struct ReferenceView: View {
     @StateObject var referenceStore : DataStore = DataStore(datas: referenceData)
     let chosenList : Datas
@@ -21,7 +24,7 @@ struct ReferenceView: View {
     }
 
 }
-
+//Preview for refrence window
 struct ReferenceView_Previews: PreviewProvider {
     static var previews: some View {
         ReferenceView(chosenList: referenceData[0])
